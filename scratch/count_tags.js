10adb05562d61d1a1022abdff527d629af9ce548
abelicio7 <abeliciosimoney@gmail.com>
2026-05-04 21:28:36 +0200

    chore: remove unused file and associated references

--- scratch/count_tags.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/pages/Checkout.tsx', 'utf8');
-const opens = (content.match(/<div/g) || []).length;
-const closes = (content.match(/<\/div>/g) || []).length;
-console.log(`Opens: ${opens}, Closes: ${closes}`);
